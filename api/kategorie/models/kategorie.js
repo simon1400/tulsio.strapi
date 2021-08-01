b13fdc41c8f86e272f51f925e13ea58e851e1b67
Your Name <>
2021-08-01 16:43:01 +0200

    install and setting sitemap plugin

--- api/kategorie/models/kategorie.js
+++ api/kategorie/models/kategorie.js
@@ -1,22 +1,7 @@
 'use strict';
-const slugify = require('slugify');
-
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {
-  lifecycles: {
-    beforeCreate: async (data) => {
-      if (data.title) {
-        data.slug = slugify(data.title).toLowerCase();
-      }
-    },
-    beforeUpdate: async (params, data) => {
-      if (data.title) {
-        data.slug = slugify(data.title).toLowerCase();
-      }
-    },
-  },
-};
+module.exports = {};
